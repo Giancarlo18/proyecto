0df87102fdcd02fbce0d51d33a870c36942ed9ad
giancarlo18 <giankee@hotmail.com>
2017-04-19 21:42:13 -0500

    agrege titulo

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>My page</title>
+  <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+ <h1> Primera </h1>
+ <h2> primera title </h2>
+  <p class="primero"> dolor sit amet, consectetur adipiscing elit. Nulla massa tortor,
+    faucibus et dapibus non, mattis in felis. Fusce congue mattis nulla, vitae  </p>
+  <p class="principal">
+    nfnkjfnlkfjnwefjn
+  </p>
+  <a href="segunda.php"> boton2 </a>
+</body>
+</html>
---
+++ segunda.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>My page2</title>
+  <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+ <h1> segunda </h1>
+ <h2> segunda title </h2>
+  <p class="primero">
+    faucibus et dapibus non, mattis in felis. Fusce congue mattis nulla, vitae  </p>
+  <p class="principal">
+    nfnkjfnlkfjnwefjn
+  </p>
+  <a href="index.php"> boton1 </a>
+</body>
+</html>
